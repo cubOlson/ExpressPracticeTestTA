3d4951bb2fe625615d83d5c3bc51fa154f6ecec7
cubOlson <cub.olson@gmail.com>
2021-09-10 11:52:29 -0400

    Donezo

--- app.js
+++ app.js
@@ -7,7 +7,47 @@
 
 
 // Your code here
+const express = require('express');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const { HairColor, Person } = require('./models');
 
+const csrfProtection = csrf({ cookie: true });
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(cookieParser());
+app.use(express.urlencoded());
+
+app.get('/new-person', csrfProtection, asyncHandler(async(req, res) => {
+
+  const hairColors = await HairColor.findAll();
+
+  res.render('new-person', { token: req.csrfToken(), hairColors });
+}));
+
+app.post('/new-person', csrfProtection, asyncHandler(async(req, res) => {
+
+  const { firstName, lastName, age, biography, hairColorId } = req.body;
+
+  const person =  Person.create({
+    firstName, lastName, age, biography, hairColorId
+  });
+
+  res.redirect('/');
+}));
+
+app.get('/', asyncHandler(async(req, res) => {
+  const people = await Person.findAll({ include: HairColor});
+
+  res.render('list', { people })
+}));
+
+
+
+app.listen(8081, () => console.log('We are running'));
 
 
 
